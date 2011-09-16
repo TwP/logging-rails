524e3042aa4f9760f49a363a7e9558f6a3df5c66
Tim Pease <tim.pease@gmail.com>
2011-09-16 08:55:52 -0600

    Updating the module name.

--- lib/logging/rails/mixin.rb
+++ lib/logging/rails/mixin.rb
@@ -1,15 +1,29 @@
 
 module Logging::Rails
 
-  module ControllerMixin
+  # Include this module into your ApplicationController to provide
+  # per-controller log level configurability. You can also include this module
+  # into your models or anywhere else you would like to control the log level.
+  #
+  # Alternatively, you can use the `include Logging.globally` trick to add a
+  # `logger` method to every Object in the ruby interpreter. See
+  # https://github.com/TwP/logging/blob/master/lib/logging.rb#L214 for more
+  # details.
+  #
+  module Mixin
+    # This method is called when the module is included into a class. It will
+    # extend the including class so it also has a class-level `logger` method.
+    #
     def self.included( base )
       base.extend self
     end
 
+    # Returns the logger instance.
+    #
     def logger
-      @_logger ||= ::Logging::Logger[self]
+      @logger ||= ::Logging::Logger[self]
     end
-  end
+  end  # Mixin
 
-end
+end  # Logging::Rails
 
